3f2537d534c30733cfa3485e4db73ded76a167b5
Ming Wu <ming-wu@users.noreply.github.com>
2018-02-22 12:12:42 -0500

    allow 30000-40000 tcp ingress

--- main.tf
+++ main.tf
@@ -33,16 +33,23 @@ resource "aws_security_group" "minikube" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["209.112.41.194/32"]
   }
   
   ingress {
     from_port = 6443
     to_port = 6443
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["209.112.41.194/32"]
   }
 
+  ingress {
+    from_port = 30000
+    to_port = 40000
+    protocol = "tcp"
+    cidr_blocks = ["209.112.41.194/32"]
+  }
+	
   egress {
       from_port = 0
       to_port = 0
